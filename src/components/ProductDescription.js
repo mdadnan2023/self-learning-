360181f274e20e7a9db9a773aa078bbee2b36244
mdadnan2023 <adnan.md.viacon@gmail.com>
2024-04-02 17:06:35 +0530

    description page changes

--- src/components/ProductDescription.js
+++ src/components/ProductDescription.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css";
@@ -19,22 +18,56 @@ export default function ProductDescription() {
     <div className="tc ma0 pa4 min-vh-100">
       <div class="product-description-main">
         <div className="product-image-slider w-50">
+          <div className="sidebar-img-sec">
+            {clickProduct.img.map((image, index) => (
+              <figure class="sidebar-img-wrapper" key={index}>
+                <img src={image} alt="new" />
+              </figure>
+            ))}
+          </div>
           <Swiper
             spaceBetween={30}
             pagination={{ clickable: true }}
-            modules={[Pagination]}
             className="mySwiper swiper-card-wrapper"
           >
             {clickProduct.img.map((image, index) => (
               <SwiperSlide className="swiper-card" key={index}>
-                <img className="photo" src={image} alt="new" />
+                <img src={image} alt="new" />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
         <div className="product-description w-50">
-          <h1 className="product-desc-title">{clickProduct.name}</h1>
-          <h1 className="product-desc-title">{clickProduct.price}</h1>
+          <h1 className="product-desc-title">
+            {clickProduct.brand} {clickProduct.name}
+          </h1>
+          <p className="product-desc-price">Price : </p>
+          <div className="price-sec">
+            <div className="price-box">
+              <span style={{ textDecoration: "line-through" }}>
+                ₹{clickProduct.price}
+              </span>
+              <p>M.R.P</p>
+            </div>
+            <div className="price-box">
+              <span style={{ color: "#008000" }}>₹{clickProduct.price}</span>
+              <p>Selling Price</p>
+            </div>
+            <div className="price-box">
+              <span style={{ color: "red" }}>{clickProduct.price}%off</span>
+              <p>Discount</p>
+            </div>
+          </div>
+          <div className="size-sec-wrapper">
+            <p className="product-desc-price">Size : </p>
+            <div className="size-sec">
+              {clickProduct.size.map((size, index) => (
+                <button className="size-box" key={index}>
+                  {size}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
