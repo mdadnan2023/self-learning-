0eeca0dd747936d4dfc33b4c4ecb6e82bbb1beb2
Md Adnan <md.adnan@viaconteam.com>
2024-07-04 16:35:50 +0530

    login and signup using firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      console.log("Logged In Successfully");
+      alert("Logged In Successfully");
+    } catch (err) {
+      console.log(err)
+      alert("Invalid email and password")
+    }
+  };
+
   return (
     <div className="page-wrapper">
-      <div className="form-wrapper">
+      <form onSubmit={handleSubmit} className="form-wrapper">
         <label>Enter Your Email</label>
-        <input className="form-control" placeholder="email" type="email" />
+        <input
+          className="form-control"
+          placeholder="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>Enter your password</label>
         <input
           className="form-control"
           placeholder="password"
           type="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="btn btn-primary">Login</button>
-        <span style={{marginTop : "20px"}}>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+        <span style={{ marginTop: "20px" }}>
           New to Fabristore{" "}
           <Link className="btn btn-outline-primary" to="/Signup">
             SignUp
           </Link>
         </span>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,30 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log("Account created");
+      setEmail("")
+      setPassword("")
+    } catch (err) {
+      console.log(err)
+      alert("Invalid email and password")
+    }
+  };
   return (
     <div className="page-wrapper">
-      <div className="form-wrapper">
-        <label>Enter Your Name</label>
+      <form className="form-wrapper" onSubmit={handleSubmit}>
+        {/* <label>Enter Your Name</label>
         <input className="form-control" placeholder="Name" type="email" />
         <label>Enter Your Mobile Number</label>
-        <input className="form-control" placeholder="Phone" type="email" />
+        <input className="form-control" placeholder="Phone" type="email" /> */}
+        <h2>Sign up</h2>
         <label>Enter Your Email</label>
-        <input className="form-control" placeholder="Password" type="email" />
+        <input
+          className="form-control"
+          placeholder="Email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>Enter your password</label>
         <input
           className="form-control"
-          placeholder="password"
+          placeholder="Password"
           type="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="btn btn-primary">SignUp</button>
-        <span style={{marginTop : "20px"}}>
-          Existing User {" "}
+        <button type="submit" className="btn btn-primary">
+          SignUp
+        </button>
+        <span style={{ marginTop: "20px" }}>
+          Existing User{" "}
           <Link className="btn btn-outline-primary" to="/Login">
             Login
           </Link>
         </span>
-      </div>
+      </form>
     </div>
   );
 }
